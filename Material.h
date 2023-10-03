ca8ae7838e1776ef18776bfd312752992f2fbcbd
Alfredo Fonseca (RIT Student) <af7152@rit.edu>
2023-10-03 10:36:24 -0400

    Created material class for next assignment

---
+++ Material.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <memory>
+#include <DirectXMath.h>
+//#include "SimpleShader.h"
+
+class Material
+{
+public:
+	Material(DirectX::XMFLOAT3 colorTint);//,
+		//std::shared_ptr<SimpleVertexShader> vs)
+		//std::shared_ptr<SimpleVertexShader> ps)
+		//)
+private:
+
+};
+
